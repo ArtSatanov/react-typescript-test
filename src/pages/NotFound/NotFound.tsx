b25b6799849ffcb7970548fb683ff2509bc3a14a
ArtSatanov <artsatanov@gmail.com>
2024-01-13 22:43:01 +0200

    1

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,22 +1,22 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { redirect } from "react-router-dom";
-import { selectCounter } from "../../services/redux/selectors/selectors";
-import { AppDispatch } from "../../services/redux/store";
-import { countDown } from "../../services/redux/notFoundSlice/notFoundSlice";
-
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { redirect } from 'react-router-dom';
+import { selectCounter } from '../../services/redux/selectors/selectors';
+import { AppDispatch } from '../../services/redux/store';
+import { countDown } from '../../services/redux/notFoundSlice/notFoundSlice';
 
 const NotFound = () => {
   const counter = useSelector(selectCounter);
   const dispatch = useDispatch<AppDispatch>();
 
+  const onDone = () => console.log('Timer counted down');
+
   useEffect(() => {
-    counter > 0 && setTimeout((()=>dispatch(countDown())),1000)
-    
-  }, [dispatch])
+    setTimeout(() => dispatch(countDown()), 1000);
+  }, [dispatch]);
   return (
-    <div>The page you're trying to reach out hasn't been found. ${counter}</div>
-  )
-}
+    <div>The page you're trying to reach out hasn't been found. {counter}</div>
+  );
+};
 
 export default NotFound;
--- src/services/redux/notFoundSlice/notFoundSlice.ts
+++ src/services/redux/notFoundSlice/notFoundSlice.ts
@@ -1,21 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { INotFoundInitSate } from "../../../interfaces/interfaces";
+import { createSlice } from '@reduxjs/toolkit';
+import { INotFoundInitSate } from '../../../interfaces/interfaces';
+import { PayloadAction } from '@reduxjs/toolkit';
 
-const initialState : INotFoundInitSate = {
-   counter: 5,
+const initialState: INotFoundInitSate = {
+  counter: 5,
 };
 
 const notFoundSlice = createSlice({
-   name: 'notFound',
-   initialState,
-   reducers: {
-      countDown(state: INotFoundInitSate): void {
-         setTimeout(() => (state.counter = state.counter-1),1000)
-         
-      },
-   },
+  name: 'notFound',
+  initialState,
+  reducers: {
+    countDown(state: INotFoundInitSate): void {
+      state.counter -= 1;
+      console.log('action');
+    },
+  },
 });
-
 
 export const { countDown } = notFoundSlice.actions;
 export const notFoundReducer = notFoundSlice.reducer;
