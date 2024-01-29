01f6d68857e95c27f110307beaec12925e1c9d02
Artem Satanovskiy <artsatanov@gmail.com>
2024-01-29 17:07:03 +0200

    1

---
+++ src/components/MainStyles/theme/otherOverrides/OutlinedInput/OutlinedInput.ts
@@ -0,0 +1,14 @@
+import { CustomTheme } from '../../themeInterfaces';
+
+export const OutlinedInput = (theme: CustomTheme | undefined) => { 
+   return {
+      MuiOutlinedInput: {
+         styleOverrides: {
+                      notchedOutline: {
+            borderColor: theme?.palette.divider,
+          },
+         },
+      },
+   };
+
+};
--- src/components/MainStyles/theme/otherOverrides/index.ts
+++ src/components/MainStyles/theme/otherOverrides/index.ts
@@ -1,6 +1,7 @@
 import { CustomTheme } from '../themeInterfaces';
 import { CssBaseline } from './cssBaseLine/CssBaseline';
+import { OutlinedInput } from './OutlinedInput/OutlinedInput';
 
 export const otherOverrides = (theme: CustomTheme) => {
-  return Object.assign(CssBaseline(theme));
+  return Object.assign(CssBaseline(theme),OutlinedInput(theme));
 };
